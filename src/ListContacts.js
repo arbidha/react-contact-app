bec83cb680294048b10140500cad32a1a8e94c34
Arbidha <arbidha1412@gmail.com>
2021-05-04 15:21:33 -0500

    Adds 'Now showing' section with link to clear the search query

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -14,19 +14,42 @@ class ListContacts extends Component{
             query: query.trim()
         }))
     }
+
+    clearQuery = () => {
+        this.updateQuery('')
+    }
+
     render(){
+        const { query } = this.state
+        const{ contacts, onDeleteContact } = this.props
+        const showingContacts = query === '' 
+        ? contacts 
+        : contacts.filter((c) => (
+            c.name.toLowerCase().includes(query.toLowerCase())
+        ))
+
         return(
             <div className = 'list-contacts'>
-            {JSON.stringify(this.state)}
             <div className = 'list-contacts-top'>
                 <input className = 'search-contacts'
                 type='text' placeholder='Search Contacts'
-                value ={this.state.query}
+                value ={query}
                 onChange = {(event) => this.updateQuery(event.target.value)}
                 ></input>
             </div>
+            
+            {showingContacts.length !== contacts.length && (
+                <div className ='showing-contacts'>
+                    <span>Now Showing {showingContacts.length} of {contacts.length}</span>
+                    <button onClick={this.clearQuery}>show all</button>
+                    </div>
+        
+            )}
+
+
+
             <ol className= 'contact-list'>
-            { this.props.contacts.map((contact) => (
+            { showingContacts.map((contact) => (
                <li key = {contact.id} className='contact-list-item'>
                <div className='contact-avatar' 
                    style={{backgroundImage: `url(${contact.avatarURL})`
@@ -37,7 +60,7 @@ class ListContacts extends Component{
                    <p>{contact.handel}</p>
                </div>
                <button 
-               onClick = { () => this.props.onDeleteContact(contact)}
+               onClick = { () => onDeleteContact(contact)}
                className='contact-remove'> Remove</button>
                </li>
            ))}
